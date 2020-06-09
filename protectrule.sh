3eed7acf0484175fd61252a11a890c310d9046f2
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-06-09 19:12:55 +0200

    Update protectrule.sh

--- protectrule.sh
+++ protectrule.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "  {" >> waf-protectrules.json
 echo "        \"action\": \"BLOCK\",">> waf-protectrules.json
-echo "        \"key\": $1,">> waf-protectrules.json
+echo "        \"key\": $1">> waf-protectrules.json
 echo "   },">> waf-protectrules.json
