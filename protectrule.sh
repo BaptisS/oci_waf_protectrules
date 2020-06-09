fe4ba195935a6d33d5daae509185d58d7eae230d
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-06-09 18:51:04 +0200

    Create protectrule.sh

---
+++ protectrule.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "  {" >> waf-protectrules.json
+echo "        \"action\": \"BLOCK\",">> waf-protectrules.json
+echo "        \"key\": $1,">> waf-protectrules.json
+echo "        },">> waf-protectrules.json
+echo "        \"udp-options\": null">> waf-protectrules.json
+echo "      },">> waf-protectrules.json
